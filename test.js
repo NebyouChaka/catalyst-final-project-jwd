493ea0dad8d8045c6d9ba667a4d0d8765187a3f7
NebyouChaka <76010226+NebyouChaka@users.noreply.github.com>
2021-02-16 12:45:40 -0500

    Add files via upload
    we have added the testing unit to our original code @Phillip-Generation

---
+++ test.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+
+//Testing addTask
+
+describe('taskManager', () => {
+  describe('addTask', () => {
+       
+    it('should be able to add and complete taskManager', () => {
+       
+        assert.ok(['Nebyou', 'task1', 'Nebyou', 02/16/2021, 'status'].indexOf())
+    });
+});
+});
+
+
+//Testing deleteTask
+
+describe('taskManager', () => {
+    describe('deleteTask', () => {
+        it('Should be able to delete the task', () => {
+            assert.ok(['Nebyou', 'task1', 'Nebyou', 02/16/2021, 'status'].indexOf())
+        });
+    });
+});
+
+//Testing getTaskById
+
+describe('taskManager', () => {
+    describe('getTaskById', () => {
+        it('Should be able to get task by ID', () => {
+            assert.ok(['Nebyou', 'task1', 'Nebyou', 02/16/2021, 'status'].indexOf())
+        });
+    });
+});
